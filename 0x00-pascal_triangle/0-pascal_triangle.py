7270fe5a319b8fe90680cdd2606e9e3bb6d1c1c6
Froches <michaelfavour418@gmail.com>
2024-03-08 01:40:52 +0000

    pascals triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -15,11 +15,11 @@ def pascal_triangle(n):
 
     triangle = []
     for i in range(n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        if i > 0:
-            row.append(1)
-        triangle.append(row)
+        triangle.append([])
+        triangle[i].append(1)
+        if (i > 0):
+            for j in range(1, i):
+                triangle[i].append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+            triangle[i].append(1)
     
     return triangle
