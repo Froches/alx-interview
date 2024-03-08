a1aa92c12113988c7c73efadf326b332f0a7e99c
Froches <michaelfavour418@gmail.com>
2024-03-07 17:47:47 -0800

    fixed pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -21,5 +21,5 @@ def pascal_triangle(n):
             for j in range(1, i):
                 triangle[i].append(triangle[i - 1][j - 1] + triangle[i - 1][j])
             triangle[i].append(1)
-    
+
     return triangle
